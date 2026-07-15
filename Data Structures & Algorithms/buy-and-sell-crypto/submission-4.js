d2e1a387127fc091b04be07fa94240f833f2b130
Timo Cingöz <74356590+timo-cingoez@users.noreply.github.com>
2026-07-15 21:15:55 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let max = 0;
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i + 1; j < prices.length; j++) {
+                if (prices[i] > prices[j]) continue;
+                const diff = prices[j] - prices[i];
+                if (diff > max) max = diff;
+            }
+        }
+        return max;
+    }
+}
